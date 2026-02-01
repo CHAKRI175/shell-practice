0f4346c2b6fe7587743f61e36ef308f4fceeb167
chakradhar seeramsetty <chakrichakradhar148@gmail.com>
2026-02-01 18:39:00 +0530

    mysql also  togther function added

--- installing.sh
+++ installing.sh
@@ -4,8 +4,8 @@ if [ $owner -ne 0 ]; then
     echo "Please run this script as root."
     exit 1
 fi
-hobs()
-{
+hobs(){
+
     if [ $1 -ne 0 ]; then
         echo $2 installed sucessfully."
         exit 1
@@ -13,7 +13,9 @@ hobs()
         echo "$2 installation failed."
     fi
 }
+
 dnf install nginx -y
 hobs $? "nginx"
+
 dnf install mysql-server -y
 hobs $? "mysql-server"
