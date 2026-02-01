8e616c7501d8ff4f4804ec1e80b03355f9414a45
chakradhar seeramsetty <chakrichakradhar148@gmail.com>
2026-02-01 22:34:42 +0530

    modified logs

--- installing.sh
+++ installing.sh
@@ -1,21 +1,24 @@
 owner=($(id -u))
-
+logs_dir="/var/log/shell-practice.sh" 
+log_file="/var/log/shell-practice/$0.log"
 if [ $owner -ne 0 ]; then
     echo "Please run this script as root."
     exit 1
 fi
+
+mkdir -p /var/log/shell-practice
 hobs(){
 
     if [ $1 -ne 0 ]; then
-        echo "$2 installation failed."
+        echo "$2 installation failed." | tee -a $log_file
         exit 1
     else
-        echo "$2 installation successful."
+        echo "$2 installation successful." | tee -a $log_file
     fi
 }
 
-dnf install nginx -y
+dnf install nginx -y  | tee -a $log_file 
 hobs $? "nginx"
 
-dnf install mysql-server -y
+dnf install mysql-server -y | tee -a $log_file
 hobs $? "mysql-server"
