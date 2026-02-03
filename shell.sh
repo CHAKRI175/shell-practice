2d957e59b04e2584152b461bca61dadf545a9ada
chakradhar seeramsetty <chakrichakradhar148@gmail.com>
2026-02-03 12:22:41 +0530

    launching instance,,

--- shell.sh
+++ shell.sh
@@ -3,7 +3,7 @@ SG_ID="sg-092464f7c3ff76b0c"
 
 for instance in $@ 
 do
-    instance_id$ = (aws ec2 run-instances \
+    instance_id=$(aws ec2 run-instances \
         --image-id $AMI_ID \
         --instance-type t3.micro \
         --security-group-ids $SG_ID \
