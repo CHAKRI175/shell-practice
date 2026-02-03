8acc8a0194e0bfeb2d62b7ba4562cc8bea60a3f0
chakradhar seeramsetty <chakrichakradhar148@gmail.com>
2026-02-03 12:05:39 +0530

    launching instance

---
+++ shell.sh
@@ -0,0 +1,22 @@
+AMI_ID="ami-0220d79f3f480ecf5"
+SG_ID="sg-092464f7c3ff76b0c"
+
+for instance $@ 
+do
+    IP$=(aws ec2 run-instances \
+        --image-id $AMI_ID \
+        --instance-type t3.micro \
+        --security-group-ids $SG_ID \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+        --query 'Instances[*].[InstanceId, PublicIpAddress]' \
+        --output text
+        )
+    if [ $instance = "frontend" ]; then 
+        echo "Frontend instance launched with Instance ID and Public IP: $IP"
+
+    else
+        echo "IP adress of $instance : $IP"
+    fi
+
+
+done
