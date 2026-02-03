06b8930b924d971df0bd77c011edf4659beb82e0
chakradhar seeramsetty <chakrichakradhar148@gmail.com>
2026-02-03 20:16:03 +0530

    mongodb shell script

--- installing.sh
+++ installing.sh
@@ -1,24 +1,24 @@
 owner=($(id -u))
 logs_dir="/var/log/shell-practice.sh" 
 log_file="/var/log/shell-practice/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+NC="\e[0m"
 if [ $owner -ne 0 ]; then
-    echo "Please run this script as root."
+    echo "$R Please run this script as root. $NC" | tee -a $log_file
     exit 1
 fi
 
 mkdir -p /var/log/shell-practice
-hobs(){
+validate(){
 
     if [ $1 -ne 0 ]; then
-        echo "$2 installation failed." | tee -a $log_file
+        echo "$2 $R installation failed. $NC" | tee -a $log_file
         exit 1
     else
-        echo "$2 installation successful." | tee -a $log_file
+        echo "$2 $G installation successful. $NC" | tee -a $log_file
     fi
 }
 
-dnf install nginx -y  | tee -a $log_file 
-hobs $? "nginx"
-
-dnf install mysql-server -y | tee -a $log_file
-hobs $? "mysql-server"
